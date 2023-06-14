6338a53ef13f1f04c98216880a59e08336f7a767
iehtian <1850453937@qq.com>
2023-06-14 22:10:22 +0800

    快排

--- 785.cpp
+++ 785.cpp
@@ -18,29 +18,10 @@ void quicksort(int begin ,int end,int num[]){
             i++;
         }
         num[j]=num[i];
-    //     if(i<j&&num[i]!= num[j]){
-    //         swap(num[i],num[j]);
-    //     }
-    //     else if (num[i]==num[j]){
-    //         i++;
-    // }
     }
     num[i]=tag;
     quicksort(begin,i-1,num);
     quicksort(i+1, end, num);
-}
-void quick_sort(int q[], int l, int r)
-{
-    if (l >= r) return;
-
-    int i = l - 1, j = r + 1, x = q[l + r >> 1];
-    while (i < j)
-    {
-        do i ++ ; while (q[i] < x);
-        do j -- ; while (q[j] > x);
-        if (i < j) swap(q[i], q[j]);
-    }
-    quick_sort(q, l, j), quick_sort(q, j + 1, r);
 }
 int main(){
     int n;
@@ -51,7 +32,6 @@ int main(){
         scanf("%d",&num[i]);
     }
     quicksort(0,n-1,num);
-    quick_sort(num,0,n-1);
     for (int i = 0; i < n; i++){
         printf("%d ",num[i]);
     }
